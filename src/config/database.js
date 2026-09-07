e14b0a4129843b49df3a7d4ed83d14b245a72484
Grazielly Noronha Sousa <grazynoronha08@alu.ufc.com>
2026-09-07 16:02:33 -0300

    fix: corrige conexão ssl e exportação do sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,29 @@
-import { underscoredIf } from "sequelize/lib/utils";
+const configuracaoLocal = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '8108',
+  database: 'usersdb',
+  define: {
+    timestamps: true,
+    underscored: true,
+  }
+};
 
-export default{
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '8108',
-    database: 'usersdb',
-    define: {
-        timestamp: true,
-        underscored: true,
-        underscoredAll: true,
-    }
+const configuracaoNuvem = {
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: false // Desativado pois a rede interna do Render não exige
+  }
+};
 
-}
+// Define qual usar com base no ambiente (Nuvem ou PC)
+const ambienteAtual = process.env.DATABASE_URL ? configuracaoNuvem : configuracaoLocal;
+
+// Exporta de um formato híbrido que o Sequelize CLI e a sua API conseguem ler perfeitamente
+module.exports = {
+  development: configuracaoLocal,
+  production: configuracaoNuvem,
+  ...ambienteAtual
+};
