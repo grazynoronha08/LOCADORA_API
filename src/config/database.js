a7c888bfa85f58c62359bbd0c9425984a97b58c4
Grazielly Noronha Sousa <grazynoronha08@alu.ufc.com>
2026-09-07 16:07:37 -0300

    fix: atualiza sintaxe do config para ES Modules

--- src/config/database.js
+++ src/config/database.js
@@ -14,15 +14,13 @@ const configuracaoNuvem = {
   use_env_variable: 'DATABASE_URL',
   dialect: 'postgres',
   dialectOptions: {
-    ssl: false // Desativado pois a rede interna do Render não exige
+    ssl: false
   }
 };
 
-// Define qual usar com base no ambiente (Nuvem ou PC)
 const ambienteAtual = process.env.DATABASE_URL ? configuracaoNuvem : configuracaoLocal;
 
-// Exporta de um formato híbrido que o Sequelize CLI e a sua API conseguem ler perfeitamente
-module.exports = {
+export default {
   development: configuracaoLocal,
   production: configuracaoNuvem,
   ...ambienteAtual
